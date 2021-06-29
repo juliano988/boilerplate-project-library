4629169877aea6140fbb453652d2f52e410317ea
Júlio Faria <juliofariaa@gmail.com>
2021-06-29 15:06:57 -0300

    Tarefa 1 OK

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,18 @@
 
 'use strict';
 
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const bookSchema = new mongoose.Schema({
+  title: String,
+  comments: [String]
+});
+
+const Book = mongoose.model('books', bookSchema);
+
 module.exports = function (app) {
 
   app.route('/api/books')
@@ -19,6 +31,15 @@ module.exports = function (app) {
     .post(function (req, res){
       let title = req.body.title;
       //response will contain new book object including atleast _id and title
+      if(title){
+        const book = new Book({title: title});
+        book.save(function(err,doc){
+          if (err) return res.status(500).json({message: 'Internal server error'});
+          res.status(200).json({_id: doc._id, title: doc.title})
+        })
+      }else{
+        return res.status(200).send('missing required field title')
+      }
     })
     
     .delete(function(req, res){
